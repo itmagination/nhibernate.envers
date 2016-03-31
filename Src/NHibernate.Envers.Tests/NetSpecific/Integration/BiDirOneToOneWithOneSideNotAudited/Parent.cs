58137a59c239ebd4a4f8c8f02dbddb83ca91b761
ITMAGINATION <github@itmagination.pl>
2016-03-31 13:09:46 +0200

    NHE-150 Failing tests for bidirection one-to-one. One side marked as RelationTargetAuditMode.NotAudited.

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/BiDirOneToOneWithOneSideNotAudited/Parent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.BiDirOneToOneWithOneSideNotAudited
+{
+    [Audited]
+    public class Parent
+    {
+        public virtual Guid Id { get; set; }
+        [Audited(TargetAuditMode = RelationTargetAuditMode.NotAudited)]
+        public virtual Child Child { get; set; }
+
+    }
+}
